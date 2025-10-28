8878ac313b7a8414ab84f9dcbb5a64bdf8577713
Marcin Pilarczyk <marcinpilarczyk@me.com>
2025-10-28 23:25:05 +0100

    Create navigation

---
+++ tests/POMs/components/navigation.ts
@@ -0,0 +1,21 @@
+import type { Page } from "playwright";
+
+export class Navigation {
+	constructor(private readonly page: Page) {}
+
+	get selector() {
+		return {
+			deleteAccountLink: this.page.getByRole("link", { name: "Delete Account" }),
+			homeIcon: this.page.getByRole("link", { name: "Website for practice" }),
+			homeLink: this.page.getByRole("link", { name: "Home" }),
+			loggedAsInfo: this.page.getByText("Logged in as"),
+			logoutLink: this.page.getByRole("link", { name: "Signup / Login" }),
+			productsLink: this.page.getByRole("link", { name: "Products" }),
+			signupLoginLink: this.page.getByRole("link", { name: "Signup / Login" }),
+		};
+	}
+
+	navigateTo = async (locator: keyof typeof this.selector) => {
+		await this.selector[locator].click();
+	};
+}
