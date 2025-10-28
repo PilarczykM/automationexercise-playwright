39df9643391f9414013fa045d8a6c750f19ada12
Marcin Pilarczyk <marcinpilarczyk@me.com>
2025-10-28 23:27:58 +0100

    Create test fixture
    This commit will expose all pages as fixtures

---
+++ tests/fixtures/base.ts
@@ -0,0 +1,40 @@
+import { test as base } from "@playwright/test";
+import { AccountInformationPage } from "../POMs/AccountInformation";
+import { Navigation } from "../POMs/components/navigation";
+import { HomePage } from "../POMs/HomePage";
+import { SignUpLoginPage } from "../POMs/SignupLoginPage";
+import { SignupPage } from "../POMs/SignupPage";
+
+type TestFixtures = {
+	signupLoginPage: SignUpLoginPage;
+	homePage: HomePage;
+	signupPage: SignupPage;
+	accountInformationPage: AccountInformationPage;
+};
+
+export const test = base.extend<TestFixtures>({
+	accountInformationPage: async ({ page }, use) => {
+		const accountInformationPage = new AccountInformationPage(page);
+		await use(accountInformationPage);
+	},
+	homePage: async ({ page }, use) => {
+		await page.goto("/");
+		await page.getByRole("button", { name: "Consent" }).click();
+		const navigation = new Navigation(page);
+		const homePage = new HomePage(page, navigation);
+		await use(homePage);
+	},
+
+	signupLoginPage: async ({ page }, use) => {
+		const navigation = new Navigation(page);
+		const signupLoginPage = new SignUpLoginPage(page, navigation);
+		await use(signupLoginPage);
+	},
+
+	signupPage: async ({ page }, use) => {
+		const signupPage = new SignupPage(page);
+		await use(signupPage);
+	},
+});
+
+export { expect } from "@playwright/test";
